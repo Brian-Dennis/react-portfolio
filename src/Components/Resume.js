625527418241342acc6e94319830b9f619b94737
Brian <briandennis49@outlook.com>
2017-09-11 13:05:19 -0700

    making changes to work skills data chart

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -60,7 +60,7 @@ class Resume extends Component {
                       <h1><span>Skills</span></h1>
                   </div>
                   <div className="nine columns main-col">
-                      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                      <p>Below are a list of my skills in a more graphical display rather then just describing the experience I have I made a bar graph for my experience level in each on of the topics below.
                       </p>
                       <div className="bars">
                           <ul className="skills">
